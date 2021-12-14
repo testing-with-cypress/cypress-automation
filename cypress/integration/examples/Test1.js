370712e378f82eae196d985a89b3ff1d3252f1ce
Adam Morsi <adam.morsi@yahoo.com>
2021-12-14 16:15:53 +0200

    trigger click event listener

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -4,5 +4,8 @@ describe('My First Test Suite', () => {
         cy.get('.search-keyword').type('ca');
         cy.wait(2000);
         cy.get('.product:visible').should('have.length', 4);
+        //parent child selector
+        cy.get('.products').find('.product').should('have.length', 4);
+        cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
     })
 })
