df411f8741b81b182b76994c052973e671c0b096
Adam Morsi <adam.morsi@yahoo.com>
2021-12-14 17:38:05 +0200

    use each loop

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -7,5 +7,12 @@ describe('My First Test Suite', () => {
         //parent child selector
         cy.get('.products').find('.product').should('have.length', 4);
         cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text();
+
+            if(textVeg.includes('Cashews')) {
+                cy.wrap($el).find('button').click();
+            }
+        })
     })
 })
