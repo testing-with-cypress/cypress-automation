896e5169fdb2f8f35c366d852f5c3d7b3fa0edfe
Adam Morsi <adam.morsi@yahoo.com>
2021-12-14 15:30:15 +0200

    test search input

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,8 @@
 describe('My First Test Suite', () => {
-    it('My First Test Case', () => {
-        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    it('Test search functionality', () => {
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+        cy.get('.product:visible').should('have.length', 4);
     })
 })
