d70c7cf276046442c8b9ca985b6906e4e6cf63e7
Adam Morsi <adammor@amdocs.com>
2021-12-15 11:24:32 +0200

    check if the required selector has the correct text

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -4,8 +4,9 @@ describe('My First Test Suite', () => {
         cy.get('.search-keyword').type('ca');
         cy.wait(2000);
         cy.get('.product:visible').should('have.length', 4);
-        //create alias for products
+        //aliases
         cy.get('.products').as('productsWrapper');
+        cy.get('.brand').as('logo');
         //parent child selector
         cy.get('@productsWrapper').find('.product').should('have.length', 4);
         cy.get('@productsWrapper').find('.product').eq(1).contains('ADD TO CART').click();
@@ -16,6 +17,9 @@ describe('My First Test Suite', () => {
                 cy.wrap($el).find('button').click();
             }
         });
-        cy.get('.brand').then((el) =>  cy.log(el.text()));
+        cy.get('@logo').then((el) =>  cy.log(el.text()));
+
+        //check if the logo text is correct
+        cy.get('@logo').should('have.text', 'GREENKART');
     })
 })
