91abc1f77283ae14f49599f7db4418088a71c2bd
Adam Morsi <adammor@amdocs.com>
2021-12-15 10:34:20 +0200

    use cypress alias

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -4,15 +4,18 @@ describe('My First Test Suite', () => {
         cy.get('.search-keyword').type('ca');
         cy.wait(2000);
         cy.get('.product:visible').should('have.length', 4);
+        //create alias for products
+        cy.get('.products').as('productsWrapper');
         //parent child selector
-        cy.get('.products').find('.product').should('have.length', 4);
-        cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
-        cy.get('.products').find('.product').each(($el, index, $list) => {
+        cy.get('@productsWrapper').find('.product').should('have.length', 4);
+        cy.get('@productsWrapper').find('.product').eq(1).contains('ADD TO CART').click();
+        cy.get('@productsWrapper').find('.product').each(($el, index, $list) => {
             const textVeg = $el.find('h4.product-name').text();
 
             if(textVeg.includes('Cashews')) {
                 cy.wrap($el).find('button').click();
             }
-        })
+        });
+        cy.get('.brand').then((el) =>  cy.log(el.text()));
     })
 })
