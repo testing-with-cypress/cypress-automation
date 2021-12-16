ceddddf1c5a56295a16dc1c8f5a633c41af64fa1
Adam Morsi <adammor@amdocs.com>
2021-12-16 14:04:08 +0200

    check if the visited page is correct

--- cypress/integration/examples/Browser.js
+++ cypress/integration/examples/Browser.js
@@ -2,6 +2,7 @@ describe('Browser', () => {
     it('Open external browser tab in the current tab', () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
         cy.get('#opentab').invoke('removeAttr', 'target').click();
+        cy.url().should('include', 'rahulshettyacademy');
         cy.go('back');
     })
 })
