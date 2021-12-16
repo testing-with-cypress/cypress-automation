b629eeb12131b390e77f0c2ea6630e9fe4751242
Adam Morsi <adammor@amdocs.com>
2021-12-16 13:49:03 +0200

    go back to prev page

--- cypress/integration/examples/Browser.js
+++ cypress/integration/examples/Browser.js
@@ -2,5 +2,6 @@ describe('Browser', () => {
     it('Open external browser tab in the current tab', () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
         cy.get('#opentab').invoke('removeAttr', 'target').click();
+        cy.go('back');
     })
 })
