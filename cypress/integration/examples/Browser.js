79e99a9a5520307dbba522725b0c8a1943d2b8b0
Adam Morsi <adam.morsi@yahoo.com>
2021-12-18 12:33:35 +0200

    update title

--- cypress/integration/examples/Browser.js
+++ cypress/integration/examples/Browser.js
@@ -1,5 +1,5 @@
 describe('Browser', () => {
-    it('Open the correct link on click', () => {
+    it('Open the correct link on open new tab click', () => {
         cy.visit('/AutomationPractice/');
         cy.get('#opentab').invoke('removeAttr', 'target').click();
         cy.url().should('include', 'rahulshettyacademy');
