6b4379ea06002fd00200ce8f6367dd8b964b4153
Adam Morsi <adam.morsi@yahoo.com>
2021-12-18 14:57:28 +0200

    use fixtures

--- cypress/integration/examples/Hooks.js
+++ cypress/integration/examples/Hooks.js
@@ -1,10 +1,13 @@
 describe('Hooks', () => {
+    let data = {};
+
     before(() => {
         cy.visit('/angularpractice/');
+        cy.fixture('example').then((d) => data = d)
     });
 
     it('Form functionality', () => {
-        cy.get(':nth-child(1) > .form-control').type('Adam');
-        cy.get('#exampleFormControlSelect1').select('Female');
+        cy.get(':nth-child(1) > .form-control').type(data.name);
+        cy.get('#exampleFormControlSelect1').select(data.gender);
     })
 })
