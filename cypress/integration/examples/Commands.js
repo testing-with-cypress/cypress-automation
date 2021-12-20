fce7ba39b68e88c8a5c05bab1eac0f1de9261f2e
Adam Morsi <adam.morsi@yahoo.com>
2021-12-20 13:36:05 +0200

    test checkout functionality

--- cypress/integration/examples/Commands.js
+++ cypress/integration/examples/Commands.js
@@ -2,11 +2,24 @@ import HomePage from "../pagesSelectors/HomePage";
 
 describe('Shop', () => {
     before(() => {
+        Cypress.config('defaultCommandTimeout', 8000);
         cy.visit('/angularpractice/');
         HomePage.getShopTab().click();
     });
     it('Add multiple items to the cart', () => {
         cy.addToCart('Blackberry');
         cy.addToCart('Nokia Edge');
+    })
+    it('Checkout functionality', () => {
+        cy.get('#navbarResponsive > .navbar-nav > .nav-item > .nav-link').click();
+        cy.get(':nth-child(4) > :nth-child(5) > .btn').click();
+        cy.get('#country').type('ind');
+        cy.contains('India').click();
+        cy.get('#checkbox2').check({force: true});
+        cy.get('input[type="submit"]').click();
+        // cy.get('.alert').should('include', 'Success! Thank you! Your order will be delivered in next few weeks :-).');
+        cy.get('.alert').then((el) =>
+            expect(el.text().includes('Success! Thank you! Your order will be delivered in next few weeks :-).'))
+                .to.be.true)
     })
 })
