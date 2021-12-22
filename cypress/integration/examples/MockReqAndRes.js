807f2c96f36ff7d1fc78258f110394c361a6cdf2
Adam Morsi <adam.morsi@yahoo.com>
2021-12-22 15:30:51 +0200

    add test case for BE security

--- cypress/integration/examples/MockReqAndRes.js
+++ cypress/integration/examples/MockReqAndRes.js
@@ -1,5 +1,5 @@
 describe('HTTP requests and responses', () => {
-    before(() => {
+    beforeEach(() => {
         cy.visit('/angularAppdemo')
     })
     it('Mock request and response using INTERCEPT function', () => {
@@ -17,7 +17,22 @@ describe('HTTP requests and responses', () => {
             ]
         }).as('bookRetrievals')
         cy.get('button.btn.btn-primary').click()
-        cy.wait('@bookRetrievals');
+        cy.wait('@bookRetrievals').should(({request, response}) => {
+            cy.get('tbody tr').should('have.length', response.body.length);
+        });
         cy.get('p').should('have.text', 'Oops only 1 Book available')
+    })
+    it('Intercepting HTTP request to test security', () => {
+        cy.intercept('GET', 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty', (req) => {
+            //modify the url of the request
+            req.url = 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=adam';
+            //send the request
+            req.continue((res) => {
+                //test security
+                // expect(res.statusCode).to.equal(403);
+            });
+        }).as('dummyUrl');
+        cy.get('button.btn.btn-primary').click();
+        cy.wait('@dummyUrl');
     })
 })
